ec39a111df6253296c6ae793906a951e0b96edf4
KeithYokoma <Keith.Yokoma@gmail.com>
2013-10-30 16:14:05 +0900

    Add util method for http url connection

--- amalgam/Amalgam/src/main/java/com/amalgam/net/HttpURLConnectionUtils.java
+++ amalgam/Amalgam/src/main/java/com/amalgam/net/HttpURLConnectionUtils.java
@@ -1,16 +1,35 @@
 package com.amalgam.net;
 
+import android.os.Build;
+
 import java.net.HttpURLConnection;
 
 /**
+ * Utility for HttpURLConnection.
  */
 public class HttpURLConnectionUtils {
+    /**
+     * Do NOT instantiate this class.
+     */
     private HttpURLConnectionUtils() {}
 
+    /**
+     * @see {@link java.net.HttpURLConnection#disconnect()}
+     * @param connection
+     */
     public static final void disconnect(HttpURLConnection connection) {
         if (connection == null) {
             return;
         }
         connection.disconnect();
+    }
+
+    /**
+     * Avoid connection pool pollution on connection reuse by disabling it earlier than Froyo.
+     */
+    public static final void avoidConnectionPoolPollution() {
+        if (Build.VERSION_CODES.FROYO > Build.VERSION.SDK_INT) {
+            System.setProperty("http.keepAlive", "false");
+        }
     }
 }
