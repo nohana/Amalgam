2bd2c467a1728defb7505247c1c0db4502e78d13
KeithYokoma <Keith.Yokoma@gmail.com>
2013-11-23 18:07:03 +0900

    Add sip to pixel conversion

--- amalgam/Amalgam/src/main/java/com/amalgam/view/ViewUtils.java
+++ amalgam/Amalgam/src/main/java/com/amalgam/view/ViewUtils.java
@@ -1,11 +1,19 @@
 package com.amalgam.view;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.hardware.display.DisplayManager;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
+import com.amalgam.content.ContextUtils;
+
 public final class ViewUtils {
     private ViewUtils() {}
+
+    public static int dipToPixel(Context context, int dip) {
+        return dipToPixel(context.getResources(), dip);
+    }
 
     /**
      * Convert the dips to pixels, based on density scale
@@ -20,6 +28,10 @@ public final class ViewUtils {
         return (int) (dip * scale + 0.5f);
     }
 
+    public static float pixelToDip(Context context, int pixel) {
+        return pixelToDip(ContextUtils.getWindowManager(context), pixel);
+    }
+
     /**
      * Convert the pixels to dips, based on density scale
      * @param windowManager the window manager of the display to use the scale density of
@@ -27,10 +39,26 @@ public final class ViewUtils {
      * @return converted value(dip)
      */
     public static float pixelToDip(WindowManager windowManager, int pixel) {
-        float dip = 0;
         DisplayMetrics metrics = new DisplayMetrics();
         windowManager.getDefaultDisplay().getMetrics(metrics);
-        dip = metrics.scaledDensity * pixel;
-        return dip;
+        return metrics.scaledDensity * pixel;
+    }
+
+    public static float sipToPixel(Context context, float sip) {
+        return sipToPixel(context.getResources(), sip);
+    }
+
+    public static float sipToPixel(Resources resources, float sip) {
+        float density = resources.getDisplayMetrics().scaledDensity;
+        return sip * density;
+    }
+
+    public static float pixelToSip(Context context, float pixels) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        float scaledDensity = metrics.scaledDensity;
+        if (pixels == 0 || scaledDensity == 0) {
+            return 1;
+        }
+        return pixels/scaledDensity;
     }
 }
